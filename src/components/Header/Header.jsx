1a6f56d281d79c891c9dbb19c57baccad26c3fce
Rodolfo Oliveira <rodolfohro@gmail.com>
2023-06-17 19:40:51 -0300

    css Header e profile

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,76 +2,68 @@
 
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import profileIcon from '../../images/profileIcon.svg';
-import searchIcon from '../../images/searchIcon.svg';
+import iconProfile from '../../images/iconProfile.png';
+import iconSeach from '../../images/iconSearch.png';
 import SearchBar from '../SearchBar/SearchBar';
+import './Header.css';
 
 function Header() {
   const location = useLocation();
   const [searchVisible, setSearchVisible] = useState(false);
 
-  const renderSearchButton = () => {
-    // Verifica se a página atual é /meals ou /drinks para mostrar o ícone de pesquisa
-    if (location.pathname === '/meals' || location.pathname === '/drinks') {
-      return (
-        <>
-          <button
-            src={ searchIcon }
-            alt="icon-pesquisa"
-            data-testid="search-top-btn"
-            onClick={ () => setSearchVisible(!searchVisible) }
-            type="button"
-          />
-
-          {searchVisible && (
-            <SearchBar
-              type="text"
-              placeholder="Buscar"
-              data-testid="search-input"
-              // Aqui você pode adicionar a lógica do requisito 10
-            />
-          )}
-        </>
-      );
-    }
-    return null;
-  };
-
-  const renderTitle = () => {
-    let title = '';
-
-    // Define o título com base na rota atual
+  const getPageTitle = () => {
     switch (location.pathname) {
     case '/meals':
-      title = 'Meals';
-      break;
+      return 'Meals';
     case '/drinks':
-      title = 'Drinks';
-      break;
+      return 'Drinks';
     case '/profile':
-      title = 'Profile';
-      break;
+      return 'Profile';
     case '/done-recipes':
-      title = 'Done Recipes';
-      break;
+      return 'Done Recipes';
     case '/favorite-recipes':
-      title = 'Favorite Recipes';
-      break;
+      return 'Favorite Recipes';
     default:
-      title = '';
-      break;
+      return '';
+    }
+  };
+
+  const handleSearchClick = () => {
+    setSearchVisible(!searchVisible);
+  };
+
+  const renderContent = () => {
+    if (searchVisible) {
+      return (
+        <div className="SearchBar">
+          <SearchBar
+            type="text"
+            placeholder="Buscar"
+            data-testid="search-input"
+            style={ { color: '#7F00FF', textAlign: 'center' } }
+          />
+        </div>
+      );
     }
 
-    return <h1 data-testid="page-title">{title}</h1>;
+    return <h1 data-testid="page-title">{getPageTitle()}</h1>;
   };
 
   return (
     <header>
       <Link to="/profile">
-        <img src={ profileIcon } alt="Profile" data-testid="profile-top-btn" />
+        <img src={ iconProfile } alt="Profile" data-testid="profile-top-btn" />
       </Link>
-      {renderSearchButton()}
-      {renderTitle()}
+      <div className="search-container">
+        <img
+          src={ iconSeach }
+          alt="icon-pesquisa"
+          data-testid="search-top-btn"
+          type="button"
+          onClick={ handleSearchClick }
+        />
+        {renderContent()}
+      </div>
     </header>
   );
 }
